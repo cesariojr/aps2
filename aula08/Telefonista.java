9944f9c469e4adfc025e46b57d1adc2aa817024b
José Maria Cesário Jr <cesariojr@gmail.com>
2016-09-29 20:04:40 -0300

    Add files via upload

---
+++ aula08/Telefonista.java
@@ -0,0 +1,13 @@
+package aula08;
+
+public class Telefonista extends Funcionario {
+	private int estacaoDeTrabalho;
+	
+	public void setEstacaoDeTrabalho(int estacaoDeTrabalho) {
+		this.estacaoDeTrabalho = estacaoDeTrabalho;
+	}
+	
+	public int getEstacaoDeTrabalho() {
+		return estacaoDeTrabalho;
+	}
+}
